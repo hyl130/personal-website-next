b37b4f7524a7e3ba1a979f57bf12a7f111d60759
Hyunjo <hyl130@ucsd.edu>
2023-04-07 00:39:16 -0700

    favicon

--- pages/_document.js
+++ pages/_document.js
@@ -4,7 +4,7 @@ export default function Document() {
   const meta = {
     title: 'Hyunjo Lee Personal Website',
     description: 'Clone and deploy your own Next.js portfolio in minutes.',
-    image: 'https://assets.vercel.com/image/upload/q_auto/front/vercel/dps.png'
+    image: 'favicon.ico'
   }
 
   return (
